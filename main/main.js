8a89688a4259875765ebb34b9fe106a94f5fbfab
ELVIS1230 <61919996+ELVIS1230@users.noreply.github.com>
2023-01-29 14:57:27 -0500

    Elvis

--- main/main.js
+++ main/main.js
@@ -1,5 +1,20 @@
 const targets = document.querySelectorAll("[data-target]");
 const content = document.querySelectorAll("[data-content]");
+const ventana= document.getElementById("ventana");
+const btnCerrarModal = document.getElementById("cerrar-modal");
+const labelFrases = document.getElementById("contenedor-frases")
+let frases = [' "No se trata de donde vienes, si no de donde vas." </br></br> Ella Fitzgerald. ',
+            ' "Los sabios son los que buscan la sabiduría; los necios piensan ya en haberla encontrado."</br></br> Napoleón Bonaparte. ',
+             ' "Aprender sin reflexionar es una ocupación inútil."</br></br> Confucio ']
+const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
+
+labelFrases.innerHTML=fraseAleatoria
+
+ventana.showModal()
+
+btnCerrarModal.addEventListener("click", () => {
+    ventana.close();
+})
 
 targets.forEach(target => {
     
